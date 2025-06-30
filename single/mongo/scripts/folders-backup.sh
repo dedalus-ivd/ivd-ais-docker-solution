52d31a9f7bd28395096b81c6621e9709ecb61df9
Prabhu Kanna <prabhu.m@dedalus.eu>
2025-06-30 16:05:39 +0530

    IM-277

--- single/mongo/scripts/folders-backup.sh
+++ single/mongo/scripts/folders-backup.sh
@@ -1,8 +1,10 @@
+# First, save this as /opt/dedalus/docker/test/backup_config.sh and make it executable:
+
 #!/bin/bash
 
 # Define variables
 SRC_DIR="/opt/dedalus/docker/test"
-DEST_DIR="/opt/dedalus/backups/configuration"
+DEST_DIR="/opt/dedalus/docker/backups"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_NAME="backup_$TIMESTAMP.tar.gz"
 
@@ -14,3 +16,13 @@ tar --exclude="$SRC_DIR/data" --exclude="$SRC_DIR/backups" -czf "$DEST_DIR/$BACK
 
 # Delete backups older than 7 days
 find "$DEST_DIR" -name "backup_*.tar.gz" -type f -mtime +7 -exec rm -f {} \;
+
+
+# Make the script executable:
+chmod +x /opt/dedalus/docker/test/backup_config.sh
+
+# Edit the crontab with:
+crontab -e
+
+# Then add the following line:
+0 17 * * * /opt/dedalus/docker/test/backup_config.sh >> /opt/dedalus/docker/test/backup_config.log 2>&1
